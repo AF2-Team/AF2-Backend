1708dbfc2b07db5edf4e236fd67d7dcbd38ae649
Sans <167203085+Sansalmao@users.noreply.github.com>
2026-01-01 17:57:22 -0400

    Create favorite.model.ts

---
+++ src/database/models/main/favorite.model.ts
@@ -0,0 +1,38 @@
+import { Schema } from 'mongoose';
+import { MongooseModelBase } from '@database/models/bases/mongoose.model.js';
+
+export default class FavoriteModel extends MongooseModelBase {
+    static override get modelName(): string {
+        return 'Favorite';
+    }
+
+    static override definition() {
+        return {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+                required: true,
+            },
+
+            post: {
+                type: Schema.Types.ObjectId,
+                ref: 'Post',
+                required: true,
+            },
+
+            status: {
+                type: Number,
+                default: 1,
+            },
+        };
+    }
+
+    static override applyIndices(schema: Schema): void {
+        schema.index({ user: 1 });
+        schema.index({ post: 1 });
+        schema.index({ status: 1 });
+
+        // Un post solo puede ser favorito una vez por usuario
+        schema.index({ user: 1, post: 1 }, { unique: true });
+    }
+}

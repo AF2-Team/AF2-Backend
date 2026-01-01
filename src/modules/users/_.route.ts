4b0ace0508ce052fb2f49b873dcfbd582fe06b4e
Sans <167203085+Sansalmao@users.noreply.github.com>
2026-01-01 18:07:43 -0400

    Update _.route.ts

--- src/modules/users/_.route.ts
+++ src/modules/users/_.route.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import UserController from './_.controller.js';
+import UserController from './user.controller.js';
 
 const router = Router();
 
-router.post('/', UserController.create); // POST /api/v1/users
-router.get('/', UserController.list);    // GET /api/v1/users
+router.post('/', UserController.create);
+router.get('/', UserController.list);
+
+router.get('/:userId/following', UserController.following);
+router.get('/:userId/followers', UserController.followers);
+router.get('/:userId/profile', UserController.profile);
 
 export default router;

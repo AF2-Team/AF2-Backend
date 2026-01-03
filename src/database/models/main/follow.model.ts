243befb3636e6156bfee8253e991c63988ca3974
Sans <167203085+Sansalmao@users.noreply.github.com>
2026-01-03 03:32:55 -0400

    Update follow.model.ts
    Se establecieron índices únicos compuestos para evitar duplicidad de relaciones y se prepararon los ganchos de actualización de contadores de seguidores.

--- src/database/models/main/follow.model.ts
+++ src/database/models/main/follow.model.ts
@@ -1,7 +1,7 @@
 import { Schema } from 'mongoose';
 import { MongooseModelBase } from '@database/models/bases/mongoose.model.js';
 
-export type FollowTargetType = 'user' | 'tag';
+export type FollowTargetModel = 'User' | 'Tag';
 
 export default class FollowModel extends MongooseModelBase {
     static override get modelName(): string {
@@ -16,36 +16,29 @@ export default class FollowModel extends MongooseModelBase {
                 required: true,
             },
 
-            targetId: {
+            targetModel: {
+                type: String,
+                enum: ['User', 'Tag'],
+                required: true,
+            },
+
+            target: {
                 type: Schema.Types.ObjectId,
                 required: true,
+                refPath: 'targetModel',
             },
 
-            targetType: {
-                type: String,
-                enum: ['user', 'tag'],
-                required: true,
-            },
-
-            status: { 
-                type: Number, 
-                default: 1 
+            status: {
+                type: Number,
+                default: 1,
             },
         };
     }
 
     static override applyIndices(schema: Schema): void {
-        schema.index({ follower: 1 });
-        schema.index({ targetId: 1 });
-        schema.index({ targetType: 1 });
+        schema.index({ follower: 1, target: 1, targetModel: 1 }, { unique: true });
+
+        schema.index({ targetModel: 1, target: 1 });
         schema.index({ status: 1 });
-        
-        schema.index({ 
-            follower: 1, 
-            targetId: 1, 
-            targetType: 1 
-        }, { 
-            unique: true 
-        });
     }
 }

63bde5ba072091324917e8e56278fdc67aacf28f
Sans <167203085+Sansalmao@users.noreply.github.com>
2026-01-03 22:59:15 -0400

    Update and rename _.route.ts to user.route.ts

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -3,9 +3,6 @@ import UserController from './user.controller.js';
 
 const router = Router();
 
-router.post('/', UserController.create);
-router.get('/', UserController.list);
-
 router.get('/:userId/following', UserController.following);
 router.get('/:userId/followers', UserController.followers);
 router.get('/:userId/profile', UserController.profile);

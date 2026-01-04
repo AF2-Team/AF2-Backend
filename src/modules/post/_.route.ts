1c107d8bd23ec1bdd6894ace100a1dc52cf9e40b
Sans <167203085+Sansalmao@users.noreply.github.com>
2026-01-03 22:52:44 -0400

    Update _.route.ts
    Se dejó como un endpoint simple de búsqueda global.

--- src/modules/post/_.route.ts
+++ src/modules/post/_.route.ts
@@ -7,8 +7,8 @@ router.post('/', PostController.create);
 router.post('/:postId/repost', PostController.repost);
 
 router.get('/', PostController.feed);
+router.get('/feed/combined', PostController.combinedFeed);
 router.get('/user/:userId', PostController.byUser);
 router.get('/tag/:tag', PostController.byTag);
-router.get('/feed/combined', PostController.combinedFeed);
 
 export default router;

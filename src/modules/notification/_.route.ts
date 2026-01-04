57842de77a2e4b2ba8b76f40f41db0bb30c33c09
Sans <167203085+Sansalmao@users.noreply.github.com>
2026-01-04 18:06:27 -0400

    Update and rename notification.route.ts to _.route.ts
    Corrección en nombre para la ruta

--- src/modules/notification/_.route.ts
+++ src/modules/notification/_.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import NotificationController from './notification.controller.js';
+import NotificationController from './_.controller.js';
 
 const router = Router();
 

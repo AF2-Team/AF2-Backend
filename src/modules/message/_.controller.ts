ab05c781fcc9e4376db91a6aef93336254f6c5d5
Sans <167203085+Sansalmao@users.noreply.github.com>
2026-01-03 22:49:03 -0400

    Update _.controller.ts
    Se integró al ControllerBase, eliminando manejo manual de errores y respuestas.

--- src/modules/message/_.controller.ts
+++ src/modules/message/_.controller.ts
@@ -5,13 +5,13 @@ import MessageService from './message.service.js';
 class MessageController extends ControllerBase {
     send = async (req: Request, res: Response) => {
         const result = await MessageService.sendMessage(req.body);
-        return this.created(res, result, 'Message sent');
+        return this.created(result, 'Message sent');
     };
 
     list = async (req: Request, res: Response) => {
-        const options = this.getQueryFilters(req);
+        const options = this.getQueryFilters();
         const result = await MessageService.getMessages(req.params.conversationId, options);
-        return this.success(res, result);
+        return this.success(result);
     };
 }
 

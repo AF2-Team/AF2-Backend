1042c69b341a17fffd69c15392b0adca6915d30e
Sans <167203085+Sansalmao@users.noreply.github.com>
2026-01-03 22:57:34 -0400

    Update and rename _.controller.ts to user.controller.ts

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -5,28 +5,31 @@ import UserService from './user.service.js';
 class UserController extends ControllerBase {
     following = async (req: Request, res: Response) => {
         const { userId } = req.params;
-        const options = this.getQueryFilters(req);
+        const options = this.getQueryFilters();
 
         const result = await UserService.getFollowing(userId, options);
-        return this.success(res, result);
+        return this.success(result);
     };
 
     followers = async (req: Request, res: Response) => {
         const { userId } = req.params;
-        const options = this.getQueryFilters(req);
+        const options = this.getQueryFilters();
 
         const result = await UserService.getFollowers(userId, options);
-        return this.success(res, result);
+        return this.success(result);
     };
 
     profile = async (req: Request, res: Response) => {
         const { userId } = req.params;
-        const viewerId = req.user?.id; // futuro auth
+        const viewerId = req.user?.id;
 
         const result = await UserService.getProfile(userId, viewerId);
-        if (!result) return this.notFound(res, 'User not found');
 
-        return this.success(res, result);
+        if (!result) {
+            this.throwValidationError('User not found');
+        }
+
+        return this.success(result);
     };
 }
 

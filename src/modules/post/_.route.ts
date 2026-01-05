19c8a9be38b4c214920e1b6c69904f6315d082f9
mosdanuwu <danmos2399@gmail.com>
2026-01-05 10:33:44 -0400

    add authentica global in post module

--- src/modules/post/_.route.ts
+++ src/modules/post/_.route.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import PostController from './_.controller.js';
+import { AuthMiddleware } from '@middlewares/auth.middleware.js';
 
 const router = Router();
+
+//router.use(AuthMiddleware.authenticate);
 
 router.post('/', PostController.create);
 router.post('/:postId/repost', PostController.repost);
@@ -10,5 +13,9 @@ router.get('/', PostController.feed);
 router.get('/feed/combined', PostController.combinedFeed);
 router.get('/user/:userId', PostController.byUser);
 router.get('/tag/:tag', PostController.byTag);
+
+
+//router.patch('/:postId', PostController.update);
+//router.delete('/:postId', PostController.delete);
 
 export default router;

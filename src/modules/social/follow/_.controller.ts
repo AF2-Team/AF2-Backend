caa9ce1d95666a52420cec9ef9f172fbd59fa145
Sans <167203085+Sansalmao@users.noreply.github.com>
2026-01-05 22:13:02 -0400

    Create _.controller.ts

---
+++ src/modules/social/follow/_.controller.ts
@@ -0,0 +1,62 @@
+import { ControllerBase } from '@bases/controller.base.js';
+import FollowService from './_.service.js';
+
+class FollowController extends ControllerBase {
+    followUser = async () => {
+        const user = this.getUser<{ _id: string }>();
+        if (!user) this.throwValidationError('Unauthorized');
+
+        const targetUserId = this.requireParam('userId');
+        const result = await FollowService.followUser(user._id, targetUserId);
+
+        return this.created(result, 'User followed');
+    };
+
+    unfollowUser = async () => {
+        const user = this.getUser<{ _id: string }>();
+        if (!user) this.throwValidationError('Unauthorized');
+
+        const targetUserId = this.requireParam('userId');
+        const result = await FollowService.unfollowUser(user._id, targetUserId);
+
+        return this.success(result);
+    };
+
+    followers = async () => {
+        const userId = this.requireParam('userId');
+        const options = this.getQueryFilters();
+
+        const result = await FollowService.getFollowers(userId, options);
+        return this.success(result);
+    };
+
+    following = async () => {
+        const userId = this.requireParam('userId');
+        const options = this.getQueryFilters();
+
+        const result = await FollowService.getFollowing(userId, options);
+        return this.success(result);
+    };
+
+    followTag = async () => {
+        const user = this.getUser<{ _id: string }>();
+        if (!user) this.throwValidationError('Unauthorized');
+
+        const tagId = this.requireParam('tagId');
+        const result = await FollowService.followTag(user._id, tagId);
+
+        return this.created(result, 'Tag followed');
+    };
+
+    unfollowTag = async () => {
+        const user = this.getUser<{ _id: string }>();
+        if (!user) this.throwValidationError('Unauthorized');
+
+        const tagId = this.requireParam('tagId');
+        const result = await FollowService.unfollowTag(user._id, tagId);
+
+        return this.success(result);
+    };
+}
+
+export default new FollowController();

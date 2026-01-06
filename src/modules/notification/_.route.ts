94a1e14c13cb5744f994942345b8b3c6b2ad85fe
Sans <167203085+Sansalmao@users.noreply.github.com>
2026-01-05 22:26:59 -0400

    Delete src/modules/notification directory

--- src/modules/notification/_.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import NotificationController from './_.controller.js';
-
-const router = Router();
-
-router.get('/', NotificationController.list);
-router.patch('/read', NotificationController.markRead);
-
-export default router;

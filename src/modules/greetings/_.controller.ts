d710b129cca6e94ceea19bdff2a39922f7b9b52e
alirio freytez <aliriofreytez1@gmail.com>
2026-01-09 16:15:20 -0400

    fix: eliminado módulo greetings de prueba

--- src/modules/greetings/_.controller.ts
+++
@@ -1,7 +0,0 @@
-import { ControllerBase } from '@bases/controller.base.js';
-import GreetingsService from './_.service.js';
-import { ProcessedQueryFilters } from '@rules/api-query.type.js';
-
-class GreetingsController extends ControllerBase {}
-
-export default new GreetingsController();
--- src/modules/greetings/_.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import GreetingsController from './_.controller.js';
-
-const router = Router();
-
-//router.get('/random', GreetingsController.func);
-
-export default router;

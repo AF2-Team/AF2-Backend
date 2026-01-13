38efe3c3c043421a8b8a164e751c1d34fa004b45
Sans <167203085+Sansalmao@users.noreply.github.com>
2026-01-13 18:48:25 -0400

    Create _.controller.ts

---
+++ src/modules/social/interaction/_.controller.ts
@@ -0,0 +1,39 @@
+import { ControllerBase } from '@bases/controller.base.js';
+import InteractionService from './_.service.js';
+
+class InteractionController extends ControllerBase {
+    async likePost() {
+        const user = this.getUser<{ _id: string }>();
+        const postId = this.requireParam('postId');
+        const result = await InteractionService.likePost(user!._id, postId);
+
+        this.created(result, 'Post liked');
+    }
+
+    async unlikePost() {
+        const user = this.getUser<{ _id: string }>();
+        const postId = this.requireParam('postId');
+        const result = await InteractionService.unlikePost(user!._id, postId);
+
+        this.success(result);
+    }
+
+    async createComment() {
+        const user = this.getUser<{ _id: string }>();
+        const postId = this.requireParam('postId');
+        const text = this.requireBodyField('text');
+
+        const result = await InteractionService.createComment(user!._id, postId, text);
+        this.created(result, 'Comment created');
+    }
+
+    async getComments() {
+        const postId = this.requireParam('postId');
+        const options = this.getQueryFilters();
+
+        const result = await InteractionService.getComments(postId, options);
+        this.success(result);
+    }
+}
+
+export default new InteractionController();
